c1e60fbb91d685222241e17ced536ddeed64adbb
Luciano Hanna El Adji <lucianohanna@id.uff.br>
2020-05-11 10:38:04 -0300

    Issue #1

--- main.js
+++ main.js
@@ -25,3 +25,36 @@ class NoIntermediario extends No {
         return this.filho_esquerda.contagem() + filho_direita.contagem()
     }
 }
+
+function procura(caracter, raiz) {
+    if (raiz instanceof NoIntermediario) {
+        let c = procura(caracter, raiz.filho_esquerda)
+
+        if (c == null)
+            return procura(caracter, raiz.filho_direita)
+        else
+            return c
+    }
+    else {
+        if (raiz.caracter == caracter) {
+            return raiz
+        }
+    }
+}
+
+// const raiz = new Folha(true, null, 0)
+
+
+
+// console.log(typeof raiz)
+
+
+
+const folha_e = new Folha(true, null, 0);
+const folha_d = new Folha(false, 'T', 1);
+const raiz = new NoIntermediario(folha_e, folha_d)
+
+console.log(raiz)
+
+console.log('\n\n\n\n\n\n')
+console.log(procura('T', raiz))
