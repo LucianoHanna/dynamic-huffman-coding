5bf7854726ee89deb48cd3a84302acce35286da0
Luciano Hanna El Adji <lucianohanna@id.uff.br>
2020-05-13 08:38:56 -0300

    Issue #7

--- main.js
+++ main.js
@@ -27,20 +27,35 @@ class NoIntermediario extends No {
     }
 }
 
-function procura(caracter, raiz) {
+function procura(caracter, raiz, caminho) {
     if (raiz instanceof NoIntermediario) {
-        let c = procura(caracter, raiz.filho_esquerda)
+        let c = procura(caracter, raiz.filho_esquerda, caminho + '0')
 
         if (c == null)
-            return procura(caracter, raiz.filho_direita)
+            return procura(caracter, raiz.filho_direita, caminho + '1')
         else
             return c
     }
     else {
         if (raiz.caracter == caracter) {
-            return raiz
+            return {raiz, caminho}
+        } else {
+            return null
         }
     }
+}
+
+function procuraFolhaVazia(raiz) {
+    if (raiz instanceof NoIntermediario) {
+        let c = procuraFolhaVazia(raiz.filho_esquerda)
+
+        if (c == null)
+            return procuraFolhaVazia(raiz.filho_direita)
+        else
+            return c
+    }
+    else if (raiz.vazio) 
+            return raiz
 }
 
 function insere(folhaVazia, caracter) {
@@ -49,4 +64,20 @@ function insere(folhaVazia, caracter) {
     raiz.filho_direita = folhaNova
     raiz.filho_esquerda.pai = raiz
     return raiz
+}
+
+function codificaCaracter(raiz, caracter) {
+    let obj = procura(caracter, raiz, '');
+    if (obj == null) {
+        let folhaVazia = procuraFolhaVazia(raiz)
+        if (folhaVazia.pai.filho_esquerda.vazio)
+            folhaVazia.pai.filho_esquerda = insere(folhaVazia, caracter);
+        else
+            folhaVazia.pai.filho_direita = insere(folhaVazia, caracter);
+
+        return caracter.charCodeAt(0)
+    } else {
+        obj.raiz.contagem += '1'
+        return obj.caminho
+    }
 }
