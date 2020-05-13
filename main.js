e41c612ca0ada80b4904d8e903b8aeb4617e4bdd
Luciano Hanna El Adji <lucianohanna@id.uff.br>
2020-05-13 11:01:46 -0300

    Issue #4

--- main.js
+++ main.js
@@ -16,14 +16,15 @@ class Folha extends No {
 }
 
 class NoIntermediario extends No {
-    constructor (filho_esquerda, filho_direita) {
+    constructor (filho_esquerda, filho_direita, pai) {
         super()
         this.filho_esquerda = filho_esquerda
         this.filho_direita = filho_direita
     }
 
     contagem() {
-        return this.filho_esquerda.contagem() + filho_direita.contagem()
+        this.cont = this.filho_esquerda.contagem() + this.filho_direita.contagem()
+        return this.cont
     }
 }
 
@@ -77,7 +78,79 @@ function codificaCaracter(raiz, caracter) {
 
         return caracter.charCodeAt(0)
     } else {
-        obj.raiz.contagem += '1'
+        obj.raiz.cont += 1
         return obj.caminho
     }
+}
+
+function listaOrdenadaProfundidade(raiz) {
+
+    let lista = listaOrdenadaProfundidadeAuxiliar(raiz.filho_esquerda, 0, {})
+    lista = listaOrdenadaProfundidadeAuxiliar(raiz.filho_direita, 0, lista)
+
+    let niveisDecrescente = Object.keys(lista).sort((a, b) => b - a)
+    
+    let arr = []
+    
+    for (let i = 0; i < niveisDecrescente.length; i++) 
+        arr = arr.concat(lista[niveisDecrescente[i]])
+
+    return arr
+}
+
+function listaOrdenadaProfundidadeAuxiliar(raiz, nivel, lista) {
+    let level = nivel
+    if (lista[level])
+        lista[level].push(raiz)
+    else 
+        lista[level] = [raiz]
+
+    if (raiz instanceof Folha)
+        return lista
+    else 
+        raiz.contagem()
+    
+    lista = listaOrdenadaProfundidadeAuxiliar(raiz.filho_esquerda, nivel + 1, lista)
+    return listaOrdenadaProfundidadeAuxiliar(raiz.filho_direita, nivel + 1, lista)
+}
+
+function balanceamento(raiz) {
+    let lista = listaOrdenadaProfundidade(raiz)
+
+    for (let i = 0; i < lista.length; i++) {
+        for (let j = lista.length - 1; j > i; j--) {
+            if (lista[j].cont < lista[i].cont) {
+                troca(lista[i], lista[j])
+                return true
+            }
+        }
+    }
+    return false
+}
+
+function troca(obj1, obj2) {
+    if (obj1.pai.filho_esquerda == obj1) {
+        if (obj2.pai.filho_esquerda == obj2) {
+            obj1.pai.filho_esquerda = obj2
+            obj2.pai.filho_esquerda = obj1
+        }
+        else {
+            obj1.pai.filho_esquerda = obj2
+            obj2.pai.filho_direita = obj1
+        }
+    }
+    else {
+        if (obj2.pai.filho_esquerda == obj2) {
+            obj1.pai.filho_direita = obj2
+            obj2.pai.filho_esquerda = obj1
+        }
+        else {
+            obj1.pai.filho_direita = obj2
+            obj2.pai.filho_direita = obj1
+        }
+    }
+
+    let tmp = obj1.pai
+    obj1.pai = obj2.pai
+    obj2.pai = tmp
 }
